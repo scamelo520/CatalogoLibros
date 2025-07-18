9151db06353f73c0203a166e64999408efd6d7c9
Santi <camelo.santiago9@gmail.com>
2025-07-18 13:01:53 -0500

    Conectando a GitHub

---
+++ src/main/java/com/aluracursosdesafios/catalogolibros/CatalogolibrosApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursosdesafios.catalogolibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogolibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogolibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursosdesafios/catalogolibros/CatalogolibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursosdesafios.catalogolibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogolibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
